cf43faf42d54ad3f67d035933fb0e21bc1a92d93
csc575 <s.singh.xyz@gmail.com>
2016-04-05 22:32:58 -0400

    Specified a host name in the app.listen() call. This is to make the nodejs server respond to IPv4 addresses instead of IPv6. Omiting the host name makes the server respond to IPv6 only.

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var globalAuth;
 
 app.set('port', (process.env.PORT || 80));
 app.use(express.static(__dirname + '/public'));
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), '0.0.0.0', function() {
   console.log('Node app is running on port', app.get('port'));
   // // Reference: http://syskall.com/dont-run-node-dot-js-as-root/
   // // Find out which user used sudo through the environment variable
