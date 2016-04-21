4ead4999deab0e64017dfaa6c2a80575e52b271e
csc575 <s.singh.xyz@gmail.com>
2016-04-20 23:34:48 -0400

    The script to ssh to a remote host and add public key if not already added, works!

--- addpublickey.sh
+++ addpublickey.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
-ssh root@192.168.1.12 'if [ ! -d /home/ssingh28/.ssh ];
-then mkdir /home/ssingh28/.ssh && echo "created"
+
+# create a .ssh folder for the user at the BBB if it does not exist
+ssh ssingh28@192.168.1.12 'if [ ! -d /home/ssingh0/.ssh ];
+then sudo mkdir /home/ssingh0/.ssh   
+echo "created new .ssh folder"
+else echo ".ssh folder exists"
+fi' 
+
+ssh ssingh28@192.168.1.12 'if [ ! -e /home/ssingh0/.ssh/authorized_keys ];
+then sudo touch /home/ssingh0/.ssh/authorized_keys   
+echo "created authorized_key file"
+else echo "authorized_keys file exists"
 fi'
-scp top_block.py root@192.168.1.12:~/received.py 
+
+
+#if the same public key is also present in "authorized_keys", do not add again.
+publickey=$(cat "id_rsa2.pub") 
+ssh ssingh28@192.168.1.12 "if sudo grep -q '$publickey' </home/ssingh0/.ssh/authorized_keys;
+then echo 'public key already exists'
+else echo '$publickey' | sudo tee -a /home/ssingh0/.ssh/authorized_keys  
+echo 'added public key'
+fi"
+
