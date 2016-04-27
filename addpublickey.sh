c81006e3f8d0ebbe04a0d4768b7fad428724b155
csc575 <s.singh.xyz@gmail.com>
2016-04-27 19:56:55 -0400

    addpublickey bash script will accept the IP address and username as inputs. Will change server.js to pass these inputs to the bash script later

--- addpublickey.sh
+++ addpublickey.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
+# first input is the IP address of the BBB
+# second input is the username at the BBB. Login using RSA keys will be enabled for this user at the BBB.
 
 # create a .ssh folder for the user at the BBB if it does not exist
-ssh root@192.168.1.12 'if [ ! -d /home/ssingh0/.ssh ];
-then sudo mkdir /home/ssingh0/.ssh   
+ssh root@$1 'if [ ! -d /home/$2/.ssh ];
+then sudo mkdir /home/$2/.ssh   
 echo "created new .ssh folder"
 else echo ".ssh folder exists"
 fi' 
 
-ssh root@192.168.1.12 'if [ ! -e /home/ssingh0/.ssh/authorized_keys ];
+ssh root@$1 'if [ ! -e /home/$2/.ssh/authorized_keys ];
 then sudo touch /home/ssingh0/.ssh/authorized_keys   
 echo "created authorized_key file"
 else echo "authorized_keys file exists"
@@ -16,9 +18,9 @@ fi'
 
 #if the same public key is also present in "authorized_keys", do not add again.
 publickey=$(cat "uploads/publickey") 
-ssh root@192.168.1.12 "if sudo grep -q '$publickey' </home/ssingh0/.ssh/authorized_keys;
+ssh root@$1 "if sudo grep -q '$publickey' </home/$2/.ssh/authorized_keys;
 then echo 'public key already exists'
-else echo '$publickey' | sudo tee -a /home/ssingh0/.ssh/authorized_keys  
+else echo '$publickey' | sudo tee -a /home/$2/.ssh/authorized_keys  
 echo 'added public key'
 fi"
 
