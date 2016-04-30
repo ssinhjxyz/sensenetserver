fb522756be607bf7078ffffa131dcedf8e507644
Live session user <s.singh.xyz@gmail.com>
2016-04-30 16:43:48 -0400

    fixed a bug in deleteuser.sh

--- deleteuser.sh
+++ deleteuser.sh
@@ -2,5 +2,5 @@
 # first input is the IP address of the BBB
 # second input is the username to be deleted
 
-ssh sensenetserver@$1 "sudo killall --user $2 ; userdel -f $2" 
+ssh sensenetserver@$1 "sudo killall --user $2 ; sudo userdel -f $2" 
 
