0754bb88aa4f000911a844ef8de4e4013a0733ee
Live session user <s.singh.xyz@gmail.com>
2016-05-04 03:35:55 -0400

    removed confusing alphabets from the password - l,L,i,I,1

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ function storeToken(token) {
 function makeRandomString(len)
 {
     var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var possible = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz23456789";
 
     for( var i=0; i < len; i++ )
         text += possible.charAt(Math.floor(Math.random() * possible.length));
