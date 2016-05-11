286f0401121e2770592830ccab2fe8565353204a
Live session user <s.singh.xyz@gmail.com>
2016-05-11 03:40:35 -0400

    Used usermod to disable/enable login instead of userdel

--- server/adduser.sh
+++ server/adduser.sh
@@ -3,9 +3,19 @@
 # second input is the password
 # third input is the username to be created at the BBB.
 
-ssh access_server@$1 "sudo /usr/sbin/useradd -m -p $2 $3; if sudo grep -q '$3 ALL=ALL' </etc/sudoers;
-then echo 'user exists in sudoers list'
-else echo '$3 ALL=ALL' | sudo tee -a /etc/sudoers  
-echo 'user added to sudoers list'
+ssh access_server@$1 
+"if id $3 >/dev/null 2>&1;
+then 
+	sudo usermod --expiredate "" $3
+	sudo passwd -u $3 
+else
+	sudo /usr/sbin/useradd -m -p $2 $3; 
+	if sudo grep -q '$3 ALL=ALL' </etc/sudoers;
+	then 
+		echo 'user exists in sudoers list'
+	else 
+		echo '$3 ALL=ALL' | sudo tee -a /etc/sudoers  
+		echo 'user added to sudoers list'
+	fi 
 fi" 
 
--- server/deleteuser.sh
+++ server/deleteuser.sh
@@ -2,5 +2,5 @@
 # first input is the IP address of the BBB
 # second input is the username to be deleted
 
-ssh access_server@$1 "sudo killall --user $2 ; sudo userdel -f $2" 
+ssh access_server@$1 "sudo killall --user $2 ; sudo usermod --expiredate 1 $2 ; sudo passwd -l $2" 
 
