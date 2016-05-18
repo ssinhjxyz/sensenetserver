a244ff7f3dd3aacd1e024e9d85569da6f3061d7c
ssinhjxyz <s.singh.xyz@gmail.com>
2016-05-18 18:42:10 -0400

    Fixed a bug with the reserve button - Clicking on the button was refreshing the entire page.

--- client/js/init.js
+++ client/js/init.js
@@ -3,5 +3,6 @@ $(function()
 	$('#endDateTime').data("DateTimePicker").date(moment());
 	$('#startDateTime').data("DateTimePicker").date(moment());
 	$("#reserve").prop("disabled",false);
+	$("#reserve").click(function(event){reserve(event)});
 	$("#showReservationDetails").hide();
 });
