f74db535c99785f6c52f1b0d5e4b38ba45ad604a
Simran Singh <s.singh.xyz@gmail.com>
2016-05-19 23:14:53 -0400

    Fixed bugs in adduser.sh - used single quotes instead of double

--- server/adduser.sh
+++ server/adduser.sh
@@ -5,9 +5,9 @@
 # fourth input is the unencrypted password
 ssh access_server@$1 "if id $2 >/dev/null 2>&1
 then 
-	sudo usermod --expiredate "" $3
+	sudo usermod --expiredate '' $2
 	sudo passwd -u $2
-	echo  "$2:$4" | sudo chpasswd
+	echo  '$2:$4' | sudo chpasswd
 else
 	sudo /usr/sbin/useradd -m -p $3 $2
 	if sudo grep -q '$2 ALL=ALL' </etc/sudoers;
