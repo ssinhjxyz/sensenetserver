66717c08b9a0da5be560fbedaa9093592eabcec5
Simran Singh <s.singh.xyz@gmail.com>
2016-05-19 22:09:25 -0400

    changed the password using chpaswd command. The previous approach was not working.

--- server/adduser.sh
+++ server/adduser.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 # first input is the IP address of the BBB
-# second input is the password
-# third input is the username to be created at the BBB.
-ssh access_server@$1 "if id $3 >/dev/null 2>&1
+# second input is the username to be created at the BBB
+# third input is the enrypted password.
+# fourth input is the unencrypted password
+ssh access_server@$1 "if id $2 >/dev/null 2>&1
 then 
 	sudo usermod --expiredate "" $3
-	sudo passwd -u $3 
-	echo -e "$2\n$2" | passwd $3
+	sudo passwd -u $2
+	echo  "$2:$4" | sudo chpasswd
 else
-	sudo /usr/sbin/useradd -m -p $2 $3
-
-	if sudo grep -q '$3 ALL=ALL' </etc/sudoers;
+	sudo /usr/sbin/useradd -m -p $3 $2
+	if sudo grep -q '$2 ALL=ALL' </etc/sudoers;
 	then 
 		echo 'user exists in sudoers list'
 	else 
-		echo '$3 ALL=ALL' | sudo tee -a /etc/sudoers  
+		echo '$2 ALL=ALL' | sudo tee -a /etc/sudoers  
 		echo 'user added to sudoers list'
 	fi
 fi" 
