7ee9bc5ff550935f7cab32c83e0a93156482d86b
Simran Singh <s.singh.xyz@gmail.com>
2016-05-21 00:20:56 -0400

    deleted public key at the end of reservation. This will be made conditional later.

--- server/deleteuser.sh
+++ server/deleteuser.sh
@@ -3,11 +3,13 @@
 # second input is the username to be deleted
 # third input is the filename of the public key ( if the user has selected RSA key as login method ). Otherwise, it is empty
 
-ssh access_server@$1 "sudo killall --user $2 ; sudo usermod --expiredate 1 $2 ; sudo passwd -l $2" 
+ssh access_server@$1 "sudo killall --user $2 ; sudo usermod --expiredate 1 $2 ; sudo passwd -l $2"
 if [ ! -z "$3" ]
 then 
-	echo "Deleting public key"
-	publickey=$(cat "uploads/$3") 
-	ssh access_server@$1 "sudo grep -v '$publickey' /home/$2/.ssh/authorized_keys > /home/$2/.ssh/authorized_keys.new;
-	mv /home/$2/.ssh/authorized_keys.new /home/$2/.ssh/authorized_keys"
+	echo "Deleting public key";
+	scp uploads/$3 access_server@$1:/home/access_server/$3;
+	ssh access_server@$1 "grep -v -f /home/access_server/$3  /home/$2/.ssh/authorized_keys > /home/access_server/$3auth;
+			      sudo mv /home/access_server/$3auth /home/$2/.ssh/authorized_keys;
+			      rm /home/access_server/$3;"
+	
 fi
