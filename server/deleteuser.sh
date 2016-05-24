e49d74b33bf7a728caab061c94d9d1f3b0a00269
Elson <s.singh.xyz@gmail.com>
2016-05-23 22:36:47 -0400

    gave full path for home directory of the user running the access server

--- server/deleteuser.sh
+++ server/deleteuser.sh
@@ -18,7 +18,7 @@ ssh access_server@$1 "sudo killall --user $2 ; sudo userdel $2"
 if [ ! -z "$3" ]
 then 
 	echo "Deleting public key";
-	scp server/uploads/$3 access_server@$1:/home/access_server/$3;
+	scp /home/ssingh28/sensenetserver/server/uploads/$3 access_server@$1:/home/access_server/$3;
 	ssh access_server@$1 "grep -v -f /home/access_server/$3  /home/$2/.ssh/authorized_keys > /home/access_server/$3auth;
 			      sudo mv /home/access_server/$3auth /home/$2/.ssh/authorized_keys;
 			      rm /home/access_server/$3;"
