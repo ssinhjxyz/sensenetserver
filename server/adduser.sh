1745c7223acedc789fc368eb57c6b3be5f0e4e66
Elson <s.singh.xyz@gmail.com>
2016-05-23 22:33:45 -0400

    reverting to useradd and userdel for now. Will think of a way to modify later as I am facing issues with RSA login after usermod.

--- server/adduser.sh
+++ server/adduser.sh
@@ -3,14 +3,27 @@
 # second input is the username to be created at the BBB
 # third input is the enrypted password.
 # fourth input is the unencrypted password
-ssh access_server@$1 "if id $2 >/dev/null 2>&1
-then 
-	sudo usermod --expiredate '' $2
-	sudo passwd -u $2
-	echo  '$2:$4' | sudo chpasswd
-	echo 'enabling login'
-else
-	sudo /usr/sbin/useradd -m -p $3 $2
+
+# ssh access_server@$1 "
+# if id $2 >/dev/null 2>&1
+# then 
+# 	sudo usermod --expiredate '' $2
+# 	sudo passwd -u $2
+# 	echo  '$2:$4' | sudo chpasswd
+# 	echo 'enabling login'
+# else
+# 	sudo /usr/sbin/useradd -m -p $3 $2
+# 	if sudo grep -q '$2 ALL=ALL' </etc/sudoers;
+# 	then 
+# 		echo 'user exists in sudoers list'
+# 	else 
+# 		echo '$2 ALL=ALL' | sudo tee -a /etc/sudoers  
+# 		echo 'user added to sudoers list'
+# 	fi
+# 	echo 'creating user'
+# fi" 
+
+ssh access_server@$1 "sudo /usr/sbin/useradd -m -p $3 $2
 	if sudo grep -q '$2 ALL=ALL' </etc/sudoers;
 	then 
 		echo 'user exists in sudoers list'
@@ -18,6 +31,4 @@ else
 		echo '$2 ALL=ALL' | sudo tee -a /etc/sudoers  
 		echo 'user added to sudoers list'
 	fi
-	echo 'creating user'
-fi" 
-
+	echo 'creating user'" 
