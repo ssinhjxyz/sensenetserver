91c5ae7e2398020f03a74f2b8337d321e7d3d54e
simran <s.singh.xyz@gmail.com>
2016-07-06 23:42:09 -0400

    added sudo prefix to command.

--- server/adddefaultgateway.sh
+++ server/adddefaultgateway.sh
@@ -3,6 +3,4 @@
 # second input is the IP address of the access server's interface nearest to the BBB
 # third input is the ethernet interface name of the BBB nearest to the above access server's interface
 
-ssh access_server@$1 "
-route add default gw $2 $3
-"
+ssh access_server@$1 "sudo route add default gw $2 $3"
