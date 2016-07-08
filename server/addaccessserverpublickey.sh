932a6411c1c89faf024a8d104849cd8443fae052
simran <s.singh.xyz@gmail.com>
2016-07-07 23:56:36 -0400

    removed sudo from bash script commands

--- server/addaccessserverpublickey.sh
+++ server/addaccessserverpublickey.sh
@@ -5,23 +5,23 @@
 
 # create a .ssh folder for the user at the BBB if it does not exist
 ssh root@$1 "
-if sudo [  ! -d /home/$2/.ssh ];
-then sudo mkdir /home/$2/.ssh   
+if [  ! -d /home/$2/.ssh ];
+then  mkdir /home/$2/.ssh   
 echo 'created new .ssh folder'
 else echo '.ssh folder exists'
 fi"
 
 ssh root@$1 "if [ ! -e /home/$2/.ssh/authorized_keys ];
-then sudo touch /home/$2/.ssh/authorized_keys   
+then  touch /home/$2/.ssh/authorized_keys   
 echo 'created authorized_key file'
 else echo 'authorized_keys file exists'
 fi"
 
 #if the same public key is already present in "authorized_keys", do not add again.
 publickey=$(cat "/home/ssingh28/sensenetserver/server/uploads/$3") 
-ssh root@$1 "if sudo grep -q '$publickey' </home/$2/.ssh/authorized_keys;
+ssh root@$1 "if  grep -q '$publickey' </home/$2/.ssh/authorized_keys;
 then echo 'public key already exists'
-else echo '$publickey' | sudo tee -a /home/$2/.ssh/authorized_keys  
+else echo '$publickey' |  tee -a /home/$2/.ssh/authorized_keys  
 echo 'added public key'
 fi"
 
