469dd6bb41864e86f54d0f048a1e780fb7c549c1
simran <s.singh.xyz@gmail.com>
2016-07-12 15:49:49 -0400

    fixed a typo.

--- client/js/admininit.js
+++ client/js/admininit.js
@@ -11,8 +11,9 @@ $(function()
               var numBBBs = bbbInfo.length;
               for(var i = 0; i < numBBBs; i++)
               {
-              	$("ul#bbbInfo").append('<li class="list-group-item"><div class="row"><div class="col-md-2">' + i + '</div>' +
-              							'<div class="col-md-4">' + (bbbInfo[i].IP + 1) + '</div>' + 
+
+              	$("ul#bbbInfo").append('<li class="list-group-item"><div class="row"><div class="col-md-2">' + (i+1) + '</div>' +
+              							'<div class="col-md-4">' + (bbbInfo[i].IP) + '</div>' + 
               							'<div class="col-md-3">' + bbbInfo[i].Reachability + '</div>' +
               							'<div class="col-md-3">' + bbbInfo[i].Configured + '</div></div></li>');
               }
