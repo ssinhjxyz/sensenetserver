1533fc69fd6d6038e14e3edc8f6a423258f225ef
simran <s.singh.xyz@gmail.com>
2016-07-12 01:00:43 -0400

    Sent a twilio sms

--- server/reservationcreator.js
+++ server/reservationcreator.js
@@ -2,6 +2,7 @@ var gcalInterface = require('./gcalinterface');
 var accessScheduler = require('./accessScheduler');
 var utils = require('./utils');
 var gmailInterface = require('./gmailinterface');
+var twilioInterface = require('./twiliointerface');
 
 exports.create = function(ids, emailId, startDateTime, endDateTime, login, loginMethod, uid, callback)
 {  
@@ -12,6 +13,7 @@ exports.create = function(ids, emailId, startDateTime, endDateTime, login, login
       gcalInterface.createEvents(updatedResults[0], emailId, startDateTime, endDateTime);
       var password = accessScheduler.schedule(ids, startDateTime, endDateTime, login, loginMethod, uid); 
       gmailInterface.sendMails(emailId, login, password, updatedResults[0], updatedResults[1], updatedResults[2], true, endDateTime);
+      twilioInterface.sendSms();
       callback(password, updatedResults[0], updatedResults[1], updatedResults[2], true);
    });
 }
