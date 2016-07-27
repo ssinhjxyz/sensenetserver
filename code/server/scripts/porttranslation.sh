d64d0bfc0c7f710161801ac1a4dcab0186373bfd
simran <s.singh.xyz@gmail.com>
2016-07-26 23:48:14 -0400

    updated domain name of access server

--- code/server/scripts/porttranslation.sh
+++ code/server/scripts/porttranslation.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # first input is the IP address of the BBB
-# second inpshivohamut is the port number
+# second input is the port number
 
 # check for existing entry
 #result = "$(sudo iptables -t nat -C PREROUTING -d sensenet-01.csc.ncsu.edu -p tcp --dport $2 -j DNAT --to-destination $1:22)"
 #echo "${result}"
 
-if $(sudo iptables -t nat -C PREROUTING -d sensenet-01.csc.ncsu.edu -p tcp --dport $2 -j DNAT --to-destination $1:22)
+if $(sudo iptables -t nat -C PREROUTING -d sn-access.sensenet.csc.ncsu.edu -p tcp --dport $2 -j DNAT --to-destination $1:22)
 then
 	#rule exists
 	echo "Rule already exists"
 else
-	sudo iptables -t nat -A PREROUTING -d sensenet-01.csc.ncsu.edu -p tcp --dport $2 -j DNAT --to-destination $1:22
+	sudo iptables -t nat -A PREROUTING -d sn-access.sensenet.csc.ncsu.edu -p tcp --dport $2 -j DNAT --to-destination $1:22
 	echo "rule added"
 fi
 #expr length + "$(readlink -f /etc/fstab)"
