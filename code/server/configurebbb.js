a0c6554d29429ebe45eec4d5d4e2b29b5340809e
simran <s.singh.xyz@gmail.com>
2016-07-27 00:09:23 -0400

    updated BBB Info after configuring a node.

--- code/server/configurebbb.js
+++ code/server/configurebbb.js
@@ -1,4 +1,5 @@
 var exec = require('child_process').exec;
+var BBB = require('./settings/bbbs');
 
 // Constants
 var accessServerUser = "access_server";
@@ -13,8 +14,7 @@ exports.add = function(req, res)
 {
 	var bbbIP = req.body.bbbIP;
 	var bbbPort = req.body.bbbPort;
-	console.log(bbbIP);
-	console.log(bbbPort);
+	var bbbID = req.body.bbbID;
 
 	var addUserCommand = "sh ./server/scripts/addaccessserveruser.sh " + bbbIP +  " " + accessServerUser + " " + accessServerPassword;
 	var addPublicKeyCommand = "sh ./server/scripts/addaccessserverpublickey.sh " + bbbIP + " " + accessServerUser + " " + accessServerKeyName;
@@ -36,6 +36,8 @@ exports.add = function(req, res)
 	      	if (error !== null) 
 	      	{
 			   console.log('exec error: ' + error);
+			   res.end(JSON.stringify({status:"error"}));
+			   return;
 	      	}
 	      	exec(portTranslationCommand,    	
 	    	function (error, stdout, stderr) 
@@ -44,15 +46,22 @@ exports.add = function(req, res)
 		      	if (error !== null) 
 	          	{
 				   console.log('exec error: ' + error);
+				   res.end(JSON.stringify({status:"error"}));
+				   return;
 		      	}
 		      	exec(addDefaultGatewayCommand,    	
 		    	function (error, stdout, stderr) 
 		    	{
-			      	console.log('stdout: ' + stdout);
+	    			console.log('stdout: ' + stdout);
 			      	if (error !== null) 
 		          	{
 					   console.log('exec error: ' + error);
 			      	}
+			      	BBB.Info[bbbID].IP = bbbIP;
+					BBB.Info[bbbID].Port = bbbPort;
+					BBB.Info[bbbID].Configured = true;
+					BBB.Info[bbbID].Reachability = "unknown";
+			      	res.end(JSON.stringify({status:"ok"}));
 		      	});
 	      	});
 	  	});
