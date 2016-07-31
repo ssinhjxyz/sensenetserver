5a0851c824d6676294df337442646ef1da76572f
simran <s.singh.xyz@gmail.com>
2016-07-31 05:34:14 -0400

    deleted the public key file temporarily kept at the BBB

--- code/server/scripts/deletepublickey.sh
+++ code/server/scripts/deletepublickey.sh
@@ -3,4 +3,5 @@
 	# second input is the username
 	# third input is the filename of the public key
 	scp /home/ssingh28/sensenetserver/code/server/uploads/$3 access_server@$1:/home/access_server/$3;
-	ssh access_server@$1 "sudo sh -c '> /home/$2/.ssh/authorized_keys;grep -v -f /home/access_server/$3  /home/$2/.ssh/authorized_keys; > /home/$2/.ssh/authorized_keys'";
+	ssh access_server@$1 "sudo sh -c '> /home/$2/.ssh/authorized_keys;grep -v -f /home/access_server/$3  /home/$2/.ssh/authorized_keys; > /home/$2/.ssh/authorized_keys; rm /home/access_server/$3'";
+	
